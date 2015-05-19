bc38bbf9d1a1063ed717ee9ebe40c0451e0f9e0a
DanielJohnston <macademy@danieljohnston.co.uk>
2015-05-19 13:20:29 +0100

    add link test pass

--- lib/server.rb
+++ lib/server.rb
@@ -24,4 +24,11 @@ class BookmarkManager < Sinatra::Base
     erb :index
   end
 
+  post '/links' do
+    url = params['url']
+    title = params['title']
+    Link.create(url: url, title: title)
+    redirect to('/')
+  end
+
 end
